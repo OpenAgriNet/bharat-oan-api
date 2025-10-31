77c56944db1a3cff06b8c98b8052160e8acc5852
Aditya Chhabra <aditya0chhabra@gmail.com>
2025-10-31 13:59:12 +0530

    Remove s3/aws requirements for blob storage

--- helpers/utils.py
+++ helpers/utils.py
@@ -21,16 +21,6 @@ ENCODER = tiktoken.get_encoding(os.getenv("TIKTOKEN_ENCODING", "cl100k_base"))
 
 
 
-def get_s3_client():
-    """Get S3 client."""
-    return boto3.client(
-        's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        region_name=os.getenv('AWS_REGION'),
-        endpoint_url=os.getenv("AWS_ENDPOINT_URL", None)
-    )
-
 
 def get_today_date_str() -> str:
     """Get today's date as a string in the format Monday, 23rd May 2025."""
